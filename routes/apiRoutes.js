3ef6694927af2b27f520f03bf4e21d9ce2074500
Mike Oh <michaelohw5@gmail.com>
2019-03-02 13:09:30 -0800

    another push

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,16 @@ var civicAPI = process.env.civicAPI;
 var civicQuery = `https://www.googleapis.com/civicinfo/v2/representatives?key=${civicAPI}`
 var address = `&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS`
 var civicQueryURL = civicQuery + address;
-
+var user = require("../models/user.model.js");
+router.get("/api/users", function(req, res) {
+  user.findOne({ where: })
+  .then(function(result){
+    console.log(result)
+  })
+  .catch(function(err) {
+    if (err) throw err;
+  })
+})
 router.get("/civic", function (req, res) {
   request(civicQueryURL, function (err, response, body) {
     if (!err && response.statusCode === 200) {
