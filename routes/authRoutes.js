1ed86d2ccd8e1e051c97aac6cd41095f416ce024
Mike Oh <michaelohw5@gmail.com>
2019-03-04 18:24:54 -0800

    removed bracket

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,100 +7,99 @@ var router = express.Router();
 var helpers = require("./helpers/auth.helpers");
 var routeHelpers = require("./helpers/route.helper");
 //login auth route
-router.post("/login", function(req, res) {
-  //parse response into user object
-  var user = {
-    email: req.body.email,
-    password: req.body.password
-  };
-  //find user info based on provided email
-  models.User.findOne({
-    where: {
-      email: user.email
-    }
-  })
-    .then(function(resp) {
-      //parse response
-      var currentUser = resp.dataValues.id;
-      //check if user password is valid
-      if (helpers.checkIfValidPass(resp.dataValues, user.password)) {
-        //set token expiration date
-        var expiry = new Date();
-        expiry.setDate(expiry.getDate() + 7);
-        //create token
-        var token = jwt.sign(
-          {
-            exp: parseInt(expiry.getTime() / 1000),
-            userID: resp.dataValues.id,
-            fisrtName: resp.dataValues.firstName,
-            lastName: resp.dataValues.lastName,
-            email: resp.dataValues.email,
-            scaryStuff: "OOGA BOOOGA"
-          },
-          process.env.JWT_SECRET
-        );
-        //send authorization and user info cookies
-        res
-          .cookie("token", token)
-          .cookie("user", currentUser)
-          .status(200)
-          .send("cookie set");
-      } else {
-        //send error message of incorrect password
-        routeHelpers.sendJsonError(res, new Error("WRONG PASSWORD"), 401);
-      }
+router.post("/login", function (req, res) {
+    //parse response into user object
+    var user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+    //find user info based on provided email
+    models.User.findOne({
+        where: {
+            email: user.email
+        }
     })
-    .catch(function(err) {
-      //send any error messages to the client
-      routeHelpers.sendJsonError(res, err);
-    });
+        .then(function (resp) {
+            //parse response
+            var currentUser = resp.dataValues.id;
+            //check if user password is valid
+            if (helpers.checkIfValidPass(resp.dataValues, user.password)) {
+                //set token expiration date
+                var expiry = new Date();
+                expiry.setDate(expiry.getDate() + 7);
+                //create token
+                var token = jwt.sign(
+                    {
+                        exp: parseInt(expiry.getTime() / 1000),
+                        userID: resp.dataValues.id,
+                        fisrtName: resp.dataValues.firstName,
+                        lastName: resp.dataValues.lastName,
+                        email: resp.dataValues.email,
+                        scaryStuff: "OOGA BOOOGA"
+                    },
+                    process.env.JWT_SECRET
+                );
+                //send authorization and user info cookies
+                res
+                    .cookie("token", token)
+                    .cookie("user", currentUser)
+                    .status(200)
+                    .send("cookie set");
+            } else {
+                //send error message of incorrect password
+                routeHelpers.sendJsonError(res, new Error("WRONG PASSWORD"), 401);
+            }
+        })
+        .catch(function (err) {
+            //send any error messages to the client
+            routeHelpers.sendJsonError(res, err);
+        });
 });
 
 // register a new user
-router.post("/register", function(req, res) {
-  //parse request
-  var user = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    address1: req.body.address1,
-    address2: req.body.address2,
-    city: req.body.city,
-    state: req.body.state,
-    zip: req.body.zip,
-    email: req.body.email,
-    password: req.body.password1
-  };
-  //get salt
-  var salt = helpers.getSalt();
-  //generate user instance
-  var userInstance = {
-    salt: salt,
-    email: user.email,
-    hash: helpers.getHash(salt, user.password),
-    name: user.name,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    address1: user.address1,
-    address2: user.address2,
-    city: user.city,
-    state: user.state,
-    zip: user.zip
-  };
+router.post("/register", function (req, res) {
+    //parse request
+    var user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        address1: req.body.address1,
+        address2: req.body.address2,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip,
+        email: req.body.email,
+        password: req.body.password1
+    };
+    //get salt
+    var salt = helpers.getSalt();
+    //generate user instance
+    var userInstance = {
+        salt: salt,
+        email: user.email,
+        hash: helpers.getHash(salt, user.password),
+        name: user.name,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        address1: user.address1,
+        address2: user.address2,
+        city: user.city,
+        state: user.state,
+        zip: user.zip
+    };
 
-  function createUser(userInstance){
-    //create user in database
-    models.User.create(userInstance)
-      .then(function(resp) {
-        //send success message
-        res.json({ message: "Creation Sucess!", id: resp.id })
-      })
-      .catch(function(err) {
-        //send error message
-        routeHelpers.sendJsonError(res, err);
-      });
-  }
-
+    function createUser(userInstance) {
+        //create user in database
+        models.User.create(userInstance)
+            .then(function (resp) {
+                //send success message
+                res.json({ message: "Creation Sucess!", id: resp.id })
+            })
+            .catch(function (err) {
+                //send error message
+                routeHelpers.sendJsonError(res, err);
+            });
     }
+
     function getpoliticians(userInstance, next) {
         var baseURL = `https://www.googleapis.com/civicinfo/v2/representatives?key=${process.env.civicAPI}`;
         var formattedAddress = `&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS`//`&address=${userInstance.address1} ${userInstance.address2} ${userInstance.city} ${userInstance.state} ${userInstance.zip}`;
@@ -115,14 +114,14 @@ router.post("/register", function(req, res) {
             // userInstance.senator1 = body.officials[0].name;
             // userInstance.senator2 = body.officials[1].name;
             // userInstance.ushouseRep = body.officials[2].name;
-            
+
             // var data = Object.keys(body);
             // console.log(body);
 
             // next(userInstance);
         });
     }
-    function createUser(userInstance){
+    function createUser(userInstance) {
         console.log(userInstance.salt, userInstance.hash);
 
         models.User.create(userInstance)
@@ -133,9 +132,9 @@ router.post("/register", function(req, res) {
                 routeHelpers.sendJsonError(res, err);
             })
     }
-createUser(userInstance);
+    createUser(userInstance);
     //getpoliticians(userInstance, createUser);
-    
+
 
 });
 
